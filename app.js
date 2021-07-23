4a637cd3a77dafee06375429c964af942651257d
Sam Pink <samuel.pink@outlook.com>
2021-07-23 20:00:42 +0100

    added filter for property time, some of date

--- app.js
+++ app.js
@@ -1,47 +1,87 @@
 const express = require("express");
 const sequelize = require("./database/config");
+const { Op, where } = require("sequelize");
 
 var initModels = require("./models/init-models");
+const { Sequelize } = require("./database/config");
+const moment = require("moment");
+const { months } = require("moment");
 
 var models = initModels(sequelize);
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 city = "Dubai";
-start = "2021-06-01";
+start = "2020-06-01";
 end = "2021-06-03";
 type = "week";
-months = "may";
+mon = "jan";
 atype = "1bdr";
+amenities = ["WiFi", "Parking"];
 
-var params = {
-  city: city,
-  date: { start, end },
-  flexible: { type, months },
-  apartmentType: atype,
-  amenities: ["WiFi", "Pool"],
-};
 
-app.get("/", (req, res) => {
-  models.building
-    .findOne({
-      where: {
-        city: city,
+app.get("/", async (req, res) => {
+  //date or flexible, not both
+  //do an if statement
+
+  var whereCondition = {};
+
+  if (atype.length > 0) {
+    console.log(atype);
+    whereCondition["property_type"] = atype;
+  }
+
+  //not working
+  //if (amenities) {
+  //  console.log(amenities)
+  //  whereCondition["amenities"] = sequelize.literal(`ARRAY['WiFi','Parking']::"enum_test.property_amenities"[];`);
+  //}
+
+  query_b = {
+    where: {
+      city: city,
+    },
+    include: [
+      {
+        model: models.property,
+        as: "properties",
+        where: whereCondition,
       },
-    })
-    .then((building) => {
-      id = building.id;
-      models.property
-        .findOne({
-          where: {
-            building_id: id,
-          },
-        })
-        .then((p) => {
-          res.json(p)
-        });
-    });
+    ],
+  };
+
+  //I am assuming 1 city must be selected, not both
+  const js = await models.building.findOne(query_b);
+
+  //js contains all properties matching search conditions
+
+  if (months & type) {
+    const flex = new moment(`01-${month}-2021`, "DD-MMM-YYYY"); //assume the year
+    var start_flex = flex.format("YYYY-MM-DD");
+
+    if(type == "week"){
+      var end_flex = flex.add(7, 'days')
+    }else if(type == "month"){
+      var end_flex = flex.add(1, 'M')
+    }
+    
+  }
+
+  query_a = {
+    where: {
+      check_in: start,
+      check_out: end,
+    },
+    [Op.or]: [{ check_in: start_flex }, { check_out: end_flex }],
+  };
+
+  //now to filter by avalible
+  const av = await models.reservation.findOne(query_a);
+
+  //if a proprty is in av and js its a match
+
+  res.json(av);
 });
 
 app.listen(port, () => {
