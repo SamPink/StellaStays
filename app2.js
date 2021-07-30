129435b284bc4871513914633b80da3315c4c6e3
Sam Pink <samuel.pink@outlook.com>
2021-07-30 15:48:39 +0100

    solved flex time search

--- app2.js
+++ app2.js
@@ -2,7 +2,10 @@ const express = require("express");
 const sequelize = require("./database/config");
 const { Op, where } = require("sequelize");
 
-var moment = require("moment-business-days");
+const moment = require("moment-business-days");
+const momentRange = require("moment-range");
+
+//const moment = MomentRange.extendMoment(Moment);
 
 var initModels = require("./models/init-models");
 const { Sequelize } = require("./database/config");
@@ -11,15 +14,11 @@ const reservation = require("./models/reservation");
 var models = initModels(sequelize);
 
 const app = express();
-const port = 3001;
+const port = 3200;
 
 city = "Dubai"; //"city" is mandatory to set
 atype = "2bdr"; //"apartmentType" is optional
 amenities = ["WiFi", "Parking"]; //"Amenities" filters the units that support the requested values
-
-//the program can take one of "date" or "flexible" but not both
-var start = new Date("2021-06-01");
-var end = new Date("2021-06-03");
 
 type = "week"; //flexible.type can be one of "weekend", "week" or "month"
 mon = ["may", "jun"]; //flexible.months is an array of 3-letter months, for example: ["may", "jun", ..]
@@ -36,6 +35,17 @@ app.get("/", async (req, res) => {
   if (atype.length > 0) {
     console.log(atype);
     whereCondition["property_type"] = atype;
+  }
+
+  //set working days
+  if (city == "Dubai") {
+    moment.updateLocale("ae", {
+      workingWeekdays: [0, 1, 2, 3, 4, 5],
+    });
+  } else if (city == "Montreal") {
+    moment.updateLocale("ca", {
+      workingWeekdays: [1, 2, 3, 4, 5, 6],
+    });
   }
 
   //this query filters properties by city and optional values enterd into the where condition
@@ -66,33 +76,75 @@ app.get("/", async (req, res) => {
   const js = await models.building.findOne(query_b);
 
   //for selected dates check reservations and avalibilites to make sure there is no overlap
-  start = moment("2021-04-01", "YYYY-MM-DD");
-  end = moment("2021-06-1", "YYYY-MM-DD");
+  start = moment("2021-06-01", "YYYY-MM-DD");
+  end = moment("2021-06-10", "YYYY-MM-DD");
 
-  //for flex create object of all avalible selected dates, subtract dates in query
-  /*
-    const start = moment('2018-10-27 21:40:00');
-    const end = moment('2018-10-27 22:40:00');
-    const now = moment();
+  var range = momentRange().range(start, end);
 
-    const range  = moment().range(start, end);
+  var ranges = [];
 
-    range.contains(now); // Current time is within the range therefore disable button.
-
-    https://stackoverflow.com/questions/53025135/time-availability-using-moment-js
-  */
   if (true) {
-    //flex
     js.properties.forEach((p) => {
       p.reservations.forEach((r) => {
-        console.log(r);
+        //get the rage for the reservation
+        start_r = moment(r.check_in, "YYYY-MM-DD");
+        end_r = moment(r.check_out, "YYYY-MM-DD");
+
+        var range_res = momentRange().range(start_r, end_r);
+
+        ranges.push(range_res);
       });
+
       p.availabilities.forEach((a) => {
-        console.log(a);
+        if (a.is_blocked == 1) {
+          start_r = moment(a.start_date, "YYYY-MM-DD");
+          end_r = moment(a.end_date, "YYYY-MM-DD");
+
+          var range_res = momentRange().range(start_r, end_r);
+
+          ranges.push(range_res);
+        }
+      });
+      //ranges contains dates where property is not avalible
+
+      ranges.every((r) => {
+        //need to find where property is avalible for >= n days
+        var av_ranges = range.subtract(r);
+
+        console.log(av_ranges);
+
+        start = moment(av_ranges[0].start, "YYYY-MM-DD");
+        end = moment(av_ranges[0].end, "YYYY-MM-DD");
+
+        console.log("start " + start.format("YYYY-MM-DD"));
+        console.log("End " + end.format("YYYY-MM-DD"));
+
+        var weekdays = start.businessDiff(end);
+
+        var days = end.diff(start, "days") + 1; //+1 to include first day
+
+        var weekendDays = days - weekdays;
+
+        console.log("days " + days);
+        console.log("working days " + weekdays);
+        console.log("weekend days " + weekendDays);
+
+        if (type == "week" && days >= 7) {
+          //avalibleProps.push(obj.property_id);
+          return console.log("week");
+        } else if (type == "weekend" && weekendDays >= 2) {
+          //avalibleProps.push(obj.property_id);
+          return console.log("weekend");
+        } else if (type == "month" && days >= 30) {
+          //avalibleProps.push(obj.property_id);
+          return console.log("month");
+        }
+        else{
+          return console.log("no");
+        }
       });
     });
   }
-
   //then return valid entries
 
   res.json(js);
